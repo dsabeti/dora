3ea1c38183f8bbd36d4196174d822ad666d17b4c
David Sabeti <dsabeti@pivotallabs.com>
2014-05-20 15:39:55 -0700

    Update dora.rb

--- dora.rb
+++ dora.rb
@@ -21,7 +21,8 @@ class Dora < Sinatra::Base
   use LogUtils
 
   get '/' do
-'<html><body><img width="100%" src="http://www.chillestmonkey.com/img/monkey.gif" /></body></html>'
+ "Hi I'm Dora!"
+#'<html><body><img width="100%" src="http://www.chillestmonkey.com/img/monkey.gif" /></body></html>'
   end
 
   get '/find/:filename' do
